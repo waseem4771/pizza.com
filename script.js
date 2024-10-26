c26978401953a413283977159a67791824623a45
waseem4771 <waseemkhan771771@gmail.com>
2024-10-26 14:28:10 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// Navigation Script
+document.getElementById("home").addEventListener("click", function() {
+    window.location.href = "index.html";
+});
+
+document.getElementById("login").addEventListener("click", function() {
+    window.location.href = "login.html";
+});
+
+document.getElementById("register").addEventListener("click", function() {
+    window.location.href = "register.html";
+});
+
+
+
+
+
+
